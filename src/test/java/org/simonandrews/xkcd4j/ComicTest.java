832ebd9a885f81eec5b0a875f6c2ccb93e199b5a
Simon Andrews <simon.andrews212@gmail.com>
2015-04-10 18:01:51 -0400

    Remove unused methods and imports from ComicTest

--- src/test/java/org/simonandrews/xkcd4j/ComicTest.java
+++ src/test/java/org/simonandrews/xkcd4j/ComicTest.java
@@ -1,32 +1,10 @@
 package org.simonandrews.xkcd4j;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ComicTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
 	public void canCreateComicInstance() {
 		@SuppressWarnings("unused")
 		Comic comic = new Comic(111);
